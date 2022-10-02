a071e4056a0f3d46b111f2f3f308924e8de2c7b3
ArthurPV <arthurpontiervalois@gmail.com>
2022-10-02 03:22:33 -0400

    Scanner: Add comments

--- src/lang/scanner/scanner.c
+++ src/lang/scanner/scanner.c
@@ -1,4 +1,41 @@
 /*
+ * MIT License
+ *
+ * Copyright (c) 2022 ArthurPV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <base/file.h>
+#include <base/format.h>
+#include <base/macros.h>
+#include <base/new.h>
+#include <base/option.h>
+#include <base/result.h>
+#include <base/types.h>
+#include <lang/diagnostic/diagnostic.h>
+#include <lang/diagnostic/summary.h>
+#include <lang/scanner/scanner.h>
+#include <lang/scanner/token.h>
+#include <string.h>
+
+/*
   ____
  / ___|    ___    __ _   _ __    _ __     ___   _ __
  \___ \   / __|  / _` | | '_ \  | '_ \   / _ \ | '__|
@@ -60,21 +97,7 @@ Example:
 "\n"
 
 Output: no errors
-
  */
-
-#include <base/file.h>
-#include <base/format.h>
-#include <base/macros.h>
-#include <base/new.h>
-#include <base/option.h>
-#include <base/result.h>
-#include <base/types.h>
-#include <lang/diagnostic/diagnostic.h>
-#include <lang/diagnostic/summary.h>
-#include <lang/scanner/scanner.h>
-#include <lang/scanner/token.h>
-#include <string.h>
 
 #define IS_DIGIT \
     '0'          \
