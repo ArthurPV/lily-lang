3030510a2be7a11224f452c394e2e78bcdedc1cb
ArthurPV <arthurpontiervalois@gmail.com>
2022-10-10 08:32:11 -0400

    Update

--- src/lang/scanner/scanner.c
+++ src/lang/scanner/scanner.c
@@ -575,7 +575,7 @@ next_char_by_token(struct Scanner *self, struct Token tok)
         case TokenKindOrKw:
         case TokenKindNotKw:
         case TokenKindNilKw:
-		case TokenKindNoneKw:
+        case TokenKindNoneKw:
         case TokenKindUndefKw:
         case TokenKindObjectKw:
         case TokenKindSelfKw:
