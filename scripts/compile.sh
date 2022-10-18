4900f3865dea1682cd5fdf0482dc8a472b009a18
ArthurPV <arthurpontiervalois@gmail.com>
2022-10-17 22:26:48 -0400

    Update compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Arg1 = File to compile
 # Arg2 = Path which contains include file
@@ -13,7 +13,7 @@ mkdir -p lily_cache/bin
 
 cp $3 ./lily_cache/lib
 
-LD_LIBRARY_PATH=./lily_cache/lib
-export LD_LIBRARY_PATH
+# LD_LIBRARY_PATH=./lily_cache/lib
+# export LD_LIBRARY_PATH
 
 gcc -Wall -O0 -O -O2 -O3 $1 -llily_runtime_c -I $2 -L ./lily_cache/lib -o ./lily_cache/bin/$4
